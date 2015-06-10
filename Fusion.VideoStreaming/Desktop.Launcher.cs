bc6734cdd91f77a1e6b073236925623c8e0a5a8c
206@B14 <lev.bezborodov@gmail.com>
2015-06-10 09:21:34 +0300

    Transitional architecture.

---
+++ Fusion.VideoStreaming/Desktop.Launcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Input;
+using Fusion.Mathematics;
+using Fusion.VideoStreaming;
+
+namespace Fusion.VideoStreaming
+{
+    class DesktopLauncher
+    {
+        static void Main(string[] args)
+        {
+            Assembly.LoadFrom("GraphVis.dll");
+            int id = ControlServer.AddInstance("GraphVis.Game,GraphVis");
+            Instance Instance = ControlServer.GetInstance(id);
+            if (Instance.Prepare())
+            {
+                Instance.Init();
+                Instance.Start();
+                Console.ReadLine();
+                ControlServer.CleanUp();
+            }
+        }
+    }
+}
